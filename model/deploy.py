29f53b39d561bf959249e255c47bc4b94ffe6b92
AkshatSan <aaaakshaat@gmail.com>
2023-11-17 01:00:54 +0530

    Converting the code

---
+++ model/deploy.py
@@ -0,0 +1,80 @@
+import pandas as pd
+import numpy as np
+from tensorflow import keras 
+import streamlit as st
+
+class MovieRecommender:
+    def __init__(self, base_path):
+        self.base_path = base_path
+        self.load_data()
+
+    def load_data(self):
+        """
+        Loading all the required csv
+        """
+        self.ratings = pd.read_csv(self.base_path + '//data//ratings.csv', sep='\t', encoding='latin-1', 
+                                   usecols=['user_id', 'movie_id', 'user_emb_id', 'movie_emb_id', 'rating'])
+        self.users = pd.read_csv(self.base_path + '/data/users.csv', sep='\t', encoding='latin-1', 
+                                 usecols=['user_id', 'gender', 'zipcode', 'age_desc', 'occ_desc'])
+        self.movies = pd.read_csv(self.base_path + '/data/movies.csv', sep='\t', encoding='latin-1', 
+                                  usecols=['movie_id', 'title', 'genres'])
+        self.new_model = keras.models.load_model(self.base_path + '//data//baseline2_weights.h5')
+
+    def predict_rating(self, user_id, movie_id):
+        """
+        user_id:- the user id that needs the recommendation
+        movie_id:- The movie_id which will be rated
+
+        The function accepts the user_id and movie_id and predicts the rating.
+        
+        """
+        return self.new_model.predict([np.array([user_id]), np.array([movie_id])], verbose=0)[0][0]
+
+    def recommend(self, TEST_USER):
+
+        """
+        TEST_USER:- the user_id for reccommendation
+
+        The function accepts the user_id and gives the top 20 recommendation to the user
+        """
+        user_ratings = self.ratings[self.ratings['user_id'] == TEST_USER][['user_id', 'movie_id', 'rating']]
+        recommendations = self.ratings[self.ratings['movie_id'].isin(user_ratings['movie_id']) == False][['movie_id']].drop_duplicates()
+        recommendations['prediction'] = recommendations.apply(lambda x: self.predict_rating(TEST_USER, x['movie_id']), axis=1)
+        return recommendations.sort_values(by='prediction',
+                                           ascending=False).merge(self.movies,
+                                                                  on='movie_id',
+                                                                  how='inner'
+                                                                 ).head(20)
+
+    def display_recommendations(self):
+
+
+        """
+        Streamlit function so as to give a frontend projection to user
+        """
+        st.set_page_config(layout="wide", page_title="Movie Recommendation System", page_icon="🎬")
+        st.markdown("""
+        <style>
+            .sidebar .sidebar-content {
+                background-image: linear-gradient(to bottom, #f7f7f7, #f0f0f0);
+            }
+        </style>
+        """, unsafe_allow_html=True)
+
+        # Streamlit UI
+        st.title('Movie Recommendation System')
+
+        # Sidebar for user input
+        test_user_id = st.sidebar.text_input('Enter User ID:', '1')  # Default user ID is set to 1
+        test_user_id = int(test_user_id)
+
+        if st.sidebar.button('Get Recommendations'):
+            with st.spinner('Fetching Recommendations...'):
+                recommended_movies = self.recommend(test_user_id)
+                st.subheader(f"Top 20 Movie Recommendations for User ID {test_user_id}:")
+                st.dataframe(recommended_movies)
+
+if __name__ == "__main__":
+    base_path = '..'  # Replace this with your base path
+    movie_recommender = MovieRecommender(base_path)
+    movie_recommender.display_recommendations()
--- model/model.py
+++ model/model.py
@@ -1,1 +1,39 @@
-print("Helloooo")
+from keras.layers import Input, Reshape, Dot, Embedding
+from keras.models import Model
+
+class MFCFModel:
+    def __init__(self, n_users, m_items, latent_factors):
+        """
+    The model expects
+    n_users:-No of users
+    m_items:- No of items
+    latent_factors:- No of dimensions the data has to be structures
+    Consider latent factors as features which gets picked up and are used to split .
+    
+    """
+
+        self.n_users = n_users
+        self.m_items = m_items
+        self.latent_factors = latent_factors
+        
+    def create_model(self):
+
+        """
+        This function does the main step that is matrix factorization.
+        It converts the user-movie 2d matrix of ratings into user and movie .
+        The dimension of user and movie will be decided upon the number of latent_features.
+        We are then doing dot product so this is basically application of low rank matrix factorization
+        
+        """
+        user = Input(shape=(1,))
+        P = Embedding(self.n_users, self.latent_factors, input_length=1, name='user-embed')(user)
+        P = Reshape((self.latent_factors,), name="user-reshape")(P)
+
+        movie = Input(shape=(1,))
+        Q = Embedding(self.m_items, self.latent_factors, input_length=1, name='movie-embed')(movie)
+        Q = Reshape((self.latent_factors,), name="movie-reshape")(Q)
+
+        P_dot_Q = Dot(axes=1, name="dot_product")([P, Q])
+        model = Model(inputs=[user, movie], outputs=P_dot_Q, name="output")
+
+        return model
