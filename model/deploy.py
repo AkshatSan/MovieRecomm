012419091903dc7bea773e1965f35c6e8025a746
AkshatSan <aaaakshaat@gmail.com>
2023-11-17 17:13:07 +0530

    Working on deploying file

--- model/deploy.py
+++ model/deploy.py
@@ -18,7 +18,7 @@ class MovieRecommender:
                                  usecols=['user_id', 'gender', 'zipcode', 'age_desc', 'occ_desc'])
         self.movies = pd.read_csv(self.base_path + '/data/movies.csv', sep='\t', encoding='latin-1', 
                                   usecols=['movie_id', 'title', 'genres'])
-        self.new_model = keras.models.load_model(self.base_path + '//data//baseline2_weights.h5')
+        self.new_model = keras.models.load_model(self.base_path + '//data//newmodel.h5')
 
     def predict_rating(self, user_id, movie_id):
         """
@@ -39,18 +39,19 @@ class MovieRecommender:
         """
         user_ratings = self.ratings[self.ratings['user_id'] == TEST_USER][['user_id', 'movie_id', 'rating']]
         recommendations = self.ratings[self.ratings['movie_id'].isin(user_ratings['movie_id']) == False][['movie_id']].drop_duplicates()
+        recommendations=recommendations.sample(n=500,random_state=42)
         recommendations['prediction'] = recommendations.apply(lambda x: self.predict_rating(TEST_USER, x['movie_id']), axis=1)
         return recommendations.sort_values(by='prediction',
                                            ascending=False).merge(self.movies,
                                                                   on='movie_id',
                                                                   how='inner'
-                                                                 ).head(20)
+                                                                 ).head(10)
 
     def display_recommendations(self):
 
 
         """
-        Streamlit function so as to give a frontend projection to user
+        Streamlit function so as to give a frontend experience.
         """
         st.set_page_config(layout="wide", page_title="Movie Recommendation System", page_icon="🎬")
         st.markdown("""
@@ -65,14 +66,26 @@ class MovieRecommender:
         st.title('Movie Recommendation System')
 
         # Sidebar for user input
-        test_user_id = st.sidebar.text_input('Enter User ID:', '1')  # Default user ID is set to 1
+        user_ids = self.ratings['user_id'].unique()
+        test_user_id = st.sidebar.selectbox('Select User ID:', user_ids)
+        #test_user_id = st.sidebar.text_input('Enter User ID:', '1')  # Default user ID is set to 1
         test_user_id = int(test_user_id)
 
         if st.sidebar.button('Get Recommendations'):
             with st.spinner('Fetching Recommendations...'):
                 recommended_movies = self.recommend(test_user_id)
                 st.subheader(f"Top 20 Movie Recommendations for User ID {test_user_id}:")
-                st.dataframe(recommended_movies)
+                #st.dataframe(recommended_movies)
+                recommended_movies = recommended_movies[['prediction', 'title', 'genres']]
+                styled_recommendations = recommended_movies.style\
+                .bar(subset=['prediction'], color='#FFA07A')\
+                .background_gradient(cmap='Blues', subset=['prediction'])\
+                .highlight_max(subset=['prediction'], color='lightgreen')\
+                .highlight_min(subset=['prediction'], color='lightcoral')\
+                .set_properties(**{'text-align': 'center'})
+                
+            
+            st.dataframe(styled_recommendations)
 
 if __name__ == "__main__":
     base_path = '..'  # Replace this with your base path
