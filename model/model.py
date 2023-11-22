63bfc8fff86add2a66596fa669a4838c9d932445
AkshatSan <aaaakshaat@gmail.com>
2023-11-23 01:37:13 +0530

    Adding doc strings

--- model/model.py
+++ model/model.py
@@ -4,27 +4,32 @@ from keras.models import Model
 class MFCFModel:
     def __init__(self, n_users, m_items, latent_factors):
         """
-    The model expects
-    n_users:-No of users
-    m_items:- No of items
-    latent_factors:- No of dimensions the data has to be structures
-    Consider latent factors as features which gets picked up and are used to split .
-    
-    """
+        Initializes the Matrix Factorization Collaborative Filtering (MFCF) model.
+
+        Args:
+        - n_users (int): Number of users in the dataset.
+        - m_items (int): Number of items (movies) in the dataset.
+        - latent_factors (int): Number of dimensions for latent factors.
+
+        Consider latent factors as features which get picked up and are used to split.
+        """
 
         self.n_users = n_users
         self.m_items = m_items
         self.latent_factors = latent_factors
         
     def create_model(self):
+        """
+        Creates the Matrix Factorization Collaborative Filtering (MFCF) model.
 
+        This function performs matrix factorization, converting the user-movie 2D matrix of ratings into
+        user and movie dimensions based on the number of latent features.
+        It employs a low-rank matrix factorization approach by applying dot product operations.
+
+        Returns:
+        - Model: Compiled MFCF model.
         """
-        This function does the main step that is matrix factorization.
-        It converts the user-movie 2d matrix of ratings into user and movie .
-        The dimension of user and movie will be decided upon the number of latent_features.
-        We are then doing dot product so this is basically application of low rank matrix factorization
-        
-        """
+
         user = Input(shape=(1,))
         P = Embedding(self.n_users, self.latent_factors, input_length=1, name='user-embed')(user)
         P = Reshape((self.latent_factors,), name="user-reshape")(P)
